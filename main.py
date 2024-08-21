4d84b5a74416eeaafa900c024071f324ab611dbe
Anthony Argatoff <anthonyargatoff@gmail.com>
2024-08-20 19:29:13 -0700

    initial

---
+++ main.py
@@ -0,0 +1,21 @@
+import tinytuya
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+startBulbPercentage = 0
+maxBulbPercentage = 100
+duration = 3600 # 1 hour in seconds
+
+l = tinytuya.BulbDevice(os.environ.get('DEVICE_ID'), os.environ.get('DEVICE_IP'), os.environ.get('DEVICE_KEY'), version=3.3)
+data = l.status()
+print(data)
+
+l.set_white_percentage(0,0)
+while startBulbPercentage <= 100:
+    print(startBulbPercentage)
+    l.set_brightness_percentage(startBulbPercentage)
+    startBulbPercentage += 1
+    time.sleep(duration/100) # run 100 over the duration for gradual wake up
